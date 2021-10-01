b6887e516508124545200d3760367896e75a132f
Ebubekir KARAMUSTAFA <77796622+ekaramustafa@users.noreply.github.com>
2021-10-01 13:43:08 +0000

    It takes inputs from user, and prints ID, x raised to the power y, and log(base 2) of x

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ x = int(input("Enter number x: "))
 y = int(input("Enter number y: "))
 myid = 76289
 print("x**y = " , x ** y)
-print("log(x) " , math.log(x))
+print("log(x) =" , math.log(x,2))
 print(myid)
 
